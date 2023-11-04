2fe25f40cfe66b17f2d0be98490d417ebd01ea2f
Wadzanai Murombedzi <wadzanaimurombedzi0@gmail.com>
2023-11-04 07:34:21 +0200

    Car rental

---
+++ car_rental/settings.py
@@ -0,0 +1,151 @@
+import os
+from pathlib import Path
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-q^3*pv*8gld6dg8^r_(bc@u2t^@1h*s+-s)-my+x9gj=v+4-gx'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = ['.vercel.app']
+
+
+# Application definition
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+
+    #Local 
+    'user',
+    'car_owner',
+    'chauffeur',
+    'rental',
+    'cars',
+    'home',
+    'payments',
+    'crispy_forms',
+    'rest_framework',
+    'datetimepicker',
+]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'  
+AUTH_USER_MODEL = 'user.CustomUser'
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'car_rental.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'car_rental.wsgi.application'
+
+
+# Database
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+SESSION_ENGINE = 'django.contrib.sessions.backends.db'
+
+# Password validation
+# https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+
+# Internationalization
+# https://docs.djangoproject.com/en/4.2/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/4.2/howto/static-files/
+
+BASE_URL = "landingpage"
+LOGIN_REDIRECT_URL = 'landingpage'
+LOGOUT_REDIRECT_URL = 'landingpage'
+PROJECT_ROOT = os.path.join(os.path.abspath(__file__))
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/media')
+MEDIA_URL = '/media/'
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# Default primary key field type
+# https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#Paynow 
+PAYNOW_INTEGRATION_ID = '16389'
+PAYNOW_INTEGRATION_KEY = '99b04904-d955-413c-879a-8567d3e855ba'
+PAYNOW_RETURN_URL = 'http://localhost:8000/payment/complete'
+PAYNOW_RESULT_URL = ''
+
+#SMTP Configuration
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'simbarashemutombe1@gmail.com'
+EMAIL_HOST_PASSWORD = 'Simbarashe2001'
---
+++ car_rental/urls.py
@@ -0,0 +1,20 @@
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from home.views import (
+    landingpage
+)
+
+urlpatterns = [
+    path('', landingpage, name='landingpage'),
+    path('', include("cars.urls")),
+    path('', include("chauffeur.urls")),
+    path('', include("rental.urls")),
+    path('', include("car_owner.urls")),
+    path('', include("user.urls")),
+    path('admin/', admin.site.urls),
+]
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
